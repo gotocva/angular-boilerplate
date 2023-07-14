67090e998a68d173eae5a9f7853e43394cd31639
siva-sparkout <95018559+siva-sparkout@users.noreply.github.com>
2023-07-14 10:33:21 +0530

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          "50": "#efefef",
+          "100": "#e2e2e2",
+          "200": "#c4c4c5",
+          "300": "#a1a1a2",
+          "400": "#737476",
+          "500": "#1c2128",
+          "600": "#191e24",
+          "700": "#161a20",
+          "800": "#13171b",
+          "900": "#0f1215"
+        }
+      }
+    },
+    fontFamily: {
+      'body': [
+    'Open Sans', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ],
+      'sans': [
+    'Open Sans', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ]
+    }
+  }
+}
